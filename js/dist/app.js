b042466ec3f19315b5e204f327cd390669a59a53
natac13 <sean.campbell13@gmail.com>
2015-09-12 16:35:50 -0400

    streamController is working to get the info from the API's

--- js/dist/app.js
+++ js/dist/app.js
@@ -3,16 +3,37 @@
 (function () {
 
     angular.module('Twitty', []).controller('StreamController', ['$scope', '$http', function ($scope, $http) {
-        var streams = ['freecodecamp', 'storbeck', 'terakilobyte', 'habathcx', 'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff', 'brunofin', 'comster404'];
+        var streams = ['freecodecamp', 'storbeck', 'terakilobyte', 'medrybw', 'habathcx', 'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff', 'brunofin', 'comster404'];
         var twitchUrl = 'https://api.twitch.tv/kraken/';
 
         $scope.test = 'test string';
         $scope.users = [];
 
         streams.forEach(function (stream) {
-            $http.get(twitchUrl + '/streams/' + stream).then(function (response) {
-                console.log(response.data);
+            // build a user object for each streamer
+            var user = {};
+            user.url = 'http://www.twitch.tv/' + stream;
+
+            $http.get(twitchUrl + 'streams/' + stream).then(function (response) {
+                // .then() success
+                user.account = true;
+                if (response.data.stream) {
+                    // online
+                    user.status = true;
+                } else {
+                    user.status = false;
+                }
+            }, function (response) {
+                // .then() fails
+                user.account = false;
             });
+
+            $http.get(twitchUrl + 'users/' + stream).then(function (response) {
+                user.displayName = response.data.display_name;
+                user.bio = response.data.bio;
+                user.logo = response.data.logo;
+            });
+            $scope.users.push(user);
         });
     }]);
 })();
--- js/src/app.js
+++ js/src/app.js
@@ -4,7 +4,7 @@
     angular.module('Twitty', [])
 
     .controller('StreamController', [ '$scope', '$http', ($scope, $http) => {
-        const streams = ['freecodecamp', 'storbeck', 'terakilobyte',
+        const streams = ['freecodecamp', 'storbeck', 'terakilobyte', 'medrybw',
         'habathcx', 'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff',
         'brunofin', 'comster404'];
         const twitchUrl = 'https://api.twitch.tv/kraken/';
@@ -13,11 +13,34 @@
         $scope.users = [];
 
         streams.forEach((stream) => {
-            $http.get(twitchUrl + '/streams/' + stream)
+            // build a user object for each streamer
+            let user = {};
+            user.url = 'http://www.twitch.tv/' + stream;
+
+            $http.get(twitchUrl + 'streams/' + stream)
+            .then((response) => {
+                // .then() success
+                user.account = true;
+                if(response.data.stream) {
+                    // online
+                    user.status = true;
+                } else {
+                    user.status = false;
+                }
+
+            }, (response) => {
+                // .then() fails
+                user.account = false;
+            });
+
+            $http.get(twitchUrl + 'users/' + stream)
             .then((response) =>{
-                console.log(response.data);
+                user.displayName = response.data.display_name;
+                user.bio = response.data.bio;
+                user.logo = response.data.logo;
 
             });
+            $scope.users.push(user);
         });
 
     }]);
